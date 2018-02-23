ef08b0954770fae0db2c117c7f4d7aa6fc6fa10d
18markian <reymark.delena@outlook.com>
2018-02-23 23:36:39 +0800

    Default project

---
+++ DocTrack/DocTrack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocTrack.Startup))]
+namespace DocTrack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
